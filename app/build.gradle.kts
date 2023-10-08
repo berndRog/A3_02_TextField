e7b3c6aee9c2345eb05a55a99318c888715ee7c8
rogallab <b-u.rogalla@ostfalia.de>
2023-10-08 13:53:31 +0200

    Libraries updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
 dependencies {
 
    // https://developer.android.com/jetpack/androidx/releases/activity
-   val activityCompose = "1.7.2"
+   val activityCompose = "1.8.0"
    implementation("androidx.activity:activity-compose:$activityCompose")
 
    // https://developer.android.com/jetpack/androidx/releases/core
@@ -68,11 +68,12 @@ dependencies {
    // A BOM is a Maven module that declares a set of libraries with their versions.
    // It will greatly simplify the way you define Compose library versions in your
    // Gradle dependencies block.
-   implementation(platform("androidx.compose:compose-bom:2023.09.01"))
+   implementation(platform("androidx.compose:compose-bom:2023.10.00"))
    implementation("androidx.compose.ui:ui")
    implementation("androidx.compose.ui:ui-graphics")
    implementation("androidx.compose.ui:ui-tooling-preview")
-   implementation("androidx.compose.material3:material3")
+   val material3 = "1.1.2"
+   implementation("androidx.compose.material3:material3:$material3")
 
    // https://kotlinlang.org/docs/releases.html
    val kotlinCoroutines = "1.7.3"
@@ -111,9 +112,10 @@ dependencies {
    androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$kotlinCoroutines")
 
    // To use Compose Testing
-   androidTestImplementation(platform("androidx.compose:compose-bom:2023.09.01"))
+   androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.00"))
    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
    debugImplementation("androidx.compose.ui:ui-tooling")
-   debugImplementation("androidx.compose.ui:ui-test-manifest")
+   val uiTestManifest = "1.5.3"
+   debugImplementation("androidx.compose.ui:ui-test-manifest:$uiTestManifest")
 
 }
--- app/src/main/java/de/rogallab/mobile/ui/theme/Type.kt
+++ app/src/main/java/de/rogallab/mobile/ui/theme/Type.kt
@@ -11,8 +11,8 @@ val Typography = Typography(
    bodyLarge = TextStyle(
       fontFamily = FontFamily.Default,
       fontWeight = FontWeight.Normal,
-      fontSize = 20.sp,  // 16.sp
-      lineHeight = 26.sp,  // 24.sp
+      fontSize = 16.sp,
+      lineHeight = 24.sp,
       letterSpacing = 0.5.sp
    )
    /* Other default text styles to override
